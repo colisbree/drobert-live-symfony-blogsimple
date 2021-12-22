b0fb870d492e92e8369babde8c7de862cf0dce47
Denis <d.combe@laposte.net>
2021-12-22 17:41:32 +0100

    mise en place du CRUD pour les articles

--- src/Controller/AdminArticleController.php
+++ src/Controller/AdminArticleController.php
@@ -1,7 +1,12 @@
 <?php
 namespace App\Controller;
 
+
+use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -16,6 +21,62 @@ class AdminArticleController extends AbstractController {
 
       return $this->render('admin/articles.html.twig', [
          'articles' => $articles
+      ]);
+   }
+
+   /**
+    * @Route("/admin/article/{id}/delete", name="admin_article_delete")
+    */
+   public function deleteArticle($id, ArticleRepository $articleRepository, EntityManagerInterface $entityManager)
+   {
+      $article= $articleRepository->find($id);
+      $entityManager->remove($article);
+      $entityManager->flush();
+
+      return $this->redirectToRoute('admin_articles');
+   }
+
+   /**
+    * @Route("/admin/article/insert", name="admin_article_insert")
+    */
+   public function insertArticle(Request $request, EntityManagerInterface $entityManager)
+   {
+      $article = new Article();
+
+      $articleForm = $this->createForm(ArticleType::class, $article);
+      $articleForm->handleRequest($request);
+
+      if($articleForm->isSubmitted() && $articleForm->isValid())
+      {
+         $entityManager->persist($article);
+         $entityManager->flush();
+         return $this->redirectToRoute('admin_articles');
+      }
+
+      return $this->render('admin/article_insert.html.twig', [
+         'articleForm' =>$articleForm->createView()
+      ]);
+   }
+
+   /**
+    * @Route("/admin/article/{id}/update", name="admin_article_update")
+    */
+   public function updateArticle($id, ArticleRepository $articleRepository, Request $request, EntityManagerInterface $entityManager){
+
+      $article= $articleRepository->find($id);
+
+      $articleForm = $this->createForm(ArticleType::class, $article);
+      $articleForm->handleRequest($request);
+
+      if($articleForm->isSubmitted() && $articleForm->isValid())
+      {
+         $entityManager->persist($article);
+         $entityManager->flush();
+         return $this->redirectToRoute('admin_articles');
+      }
+
+      return $this->render('admin/article_insert.html.twig', [
+         'articleForm' =>$articleForm->createView()
       ]);
    }
 }
---
+++ src/Form/ArticleType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => ['class' => 'tinymce'],
+            ])
+            ->add('publisehdAt')
+            ->add('isPublished')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'title'
+                ])
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
